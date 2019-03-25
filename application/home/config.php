2d13ed003e216a1c7f42f90b87091ed40bd62157
hanke <hanziacier@163.com>
2019-03-25 09:56:02 +0800

    session to redis

--- application/home/config.php
+++ application/home/config.php
@@ -6,16 +6,18 @@ return [
     //默认成功跳转对应的模板文件
     'dispatch_success_tmpl' => 'default/base/dispatch_jump',
     
-    
-    'session' => [
-        'id' => '',
-        // SESSION_ID的提交变量,解决flash上传跨域
-        'var_session_id' => '',
-        // SESSION 前缀
-        'prefix' => 'home',
+
+    'session'                => [
         // 驱动方式 支持redis memcache memcached
-        'type' => '',
+        'type'           => 'redis',
         // 是否自动开启 SESSION
-        'auto_start' => true,
+        'auto_start'     => true,
+        'host'          => $_SERVER['REDIS_HOST'],
+        'port'          => $_SERVER['REDIS_PORT'],
+        'password'      => $_SERVER['REDIS_PASSWORD'],
+        'select'        => 0,
+        'expire'        => 3600,
+        'timeout'       => 0,
+        'session_name'  => 'dsmall_sess_'
     ],
 ];
